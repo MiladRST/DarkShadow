48122dd44534a6ae77da5699b27e03a2d0ab5372
MiladRST <miladelectronic90@gmail.com>
2020-03-07 14:03:57 +0330

    adding my robofriends project

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Pouya Alebouye',
+    username: 'Popo',
+    email: 'Sincere@april.biz'
+  },
+  {
+    id: 2,
+    name: 'Arsalan Eghbalian',
+    username: 'Arsi',
+    email: 'Shanna@melissa.tv'
+  },
+  {
+    id: 3,
+    name: 'Mehdi Ranjbar',
+    username: 'Samantha',
+    email: 'Nathan@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Milad Rastin',
+    username: 'Karianne',
+    email: 'Julianne.OConner@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Arya Eghbalian',
+    username: 'Kamren',
+    email: 'Lucio_Hettinger@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Aryan Alebouye',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Mehdi Rastin',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Akbar',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Amoo Hojjat',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Kase  Daei',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
