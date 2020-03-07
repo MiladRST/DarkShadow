eddc68378ee1e8991b52be0038bad765c1de8d2a
MiladRST <miladelectronic90@gmail.com>
2020-03-07 15:00:59 +0330

    Updates

---
+++ precache-manifest.a8562f036d0c1ad5527741fe97a97e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01537bd8a9e241d01256c50270734e47",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "903673c20f0cf8c7a5d2",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d5ab48858dbcfa746723",
+    "url": "/RobotFriends/static/css/main.3c8b3f90.chunk.css"
+  },
+  {
+    "revision": "903673c20f0cf8c7a5d2",
+    "url": "/RobotFriends/static/js/2.ab38b299.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/RobotFriends/static/js/2.ab38b299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ab48858dbcfa746723",
+    "url": "/RobotFriends/static/js/main.3b4b0a58.chunk.js"
+  },
+  {
+    "revision": "212ea6b9f7ca24a7a7f8",
+    "url": "/RobotFriends/static/js/runtime-main.192eb063.js"
+  }
+]);
